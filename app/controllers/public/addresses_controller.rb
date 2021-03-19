99fcdf8f8165f4d33c9fb800b128a1aece00d538
ryouichi0618 <tennis.ryouichi@gmail.com>
2021-03-19 12:11:08 +0000

    [Add]gem,kaminari,refile-mini_magic,refile,font-awesome-sass,jquery-rail,bootstrapの追加、railsのversionを5.0.7=>5.2.4に変更

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,2 +1,30 @@
 class Public::AddressesController < Public::ApplicationController
+  
+  def index
+    @address_new = Address.new
+    @customer = current_user
+    address = Address.all
+    @addresses = @customer.address
+  end
+
+  def create
+    address = Address.new(address_params)
+    address.save
+    redirect_back(fallback_location: root_path)
+  end
+
+  def edit
+  end
+
+  def updete
+  end
+  
+  
+  private
+  
+  def address_params
+    params.require(:address).permit(:postal_code,:address,:name)
+  end
+
+
 end
--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,2 +1,22 @@
 class Public::OrdersController < Public::ApplicationController
+  
+  def new
+  end
+
+  def confrm
+  end
+
+  def complete
+  end
+
+  def create
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+
 end
