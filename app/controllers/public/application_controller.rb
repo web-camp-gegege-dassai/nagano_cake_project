c7cf8cf4214cbe431e6a3452c1f882eb3074db90
ryouichi0618 <tennis.ryouichi@gmail.com>
2021-03-19 17:48:08 +0000

    [Update]/controller/public/application_contlloer内のuncomment

--- app/controllers/public/application_controller.rb
+++ app/controllers/public/application_controller.rb
@@ -1,3 +1,3 @@
 class Public::ApplicationController  < ApplicationController
-  # before_action :authenticate_customer!, except: [:top,:about]
+   before_action :authenticate_customer!, except: [:top,:about]
 end
