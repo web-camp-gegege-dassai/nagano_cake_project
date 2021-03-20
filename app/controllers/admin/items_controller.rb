5cf95f9555d557f839d7eb98a73bc36a6e720b1e
Takuya3627 <tm.0630.ppp@gmail.com>
2021-03-20 13:35:33 +0000

    Initial commit

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,2 +1,43 @@
 class Admin::ItemsController < Admin::ApplicationController
+  
+  def index
+    @items = Item.all
+  end
+  
+  def new
+    @item = Item.new
+  end
+  
+  def show
+    @item = Item.find(params[:id])
+  end
+  
+  def edit
+    @item = Item.find(params[:id])
+  end
+  
+  def create
+    item = Item.new(item_params)
+    if item.save
+      redirect_to item_path(@item), notice: 'Item was successfully created.'
+    else
+      @items = Item.all
+      render :index
+    end
+  end
+  
+  def update
+    if @item.update(item_params)
+      redirect_to item_path(@item), notice: "You have updated item successfully."
+    else
+      render :edit
+    end
+  end
+  
+  private
+  
+  def item_params
+    params.require(:item).permit(:genre, :name, :introduction, :image, :price, :is_active)
+  end
+  
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,10 @@ class Item < ApplicationRecord
   has_many :order_details, dependent: :destroy
   has_many :cart_items, dependent: :destroy
   belongs_to :genre
+  
+  attachment :image
+  
+  validates :name, presence: true
+  validates :introduction, presence: true
+  
 end
