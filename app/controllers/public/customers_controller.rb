0c89944022e3b629792282f726c16407168e05df
chonasan <tk24120824@gmail.com>
2021-03-20 05:34:30 +0000

    first

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -24,6 +24,15 @@ class Public::CustomersController < Public::ApplicationController
    
    def withdraw
    end
+   
+   
+   
+    private
+
+      def user_params
+        params.require(:user).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postal_code, :address, :telephone_number, :is_deleted)
+      end
+
     
     
     
