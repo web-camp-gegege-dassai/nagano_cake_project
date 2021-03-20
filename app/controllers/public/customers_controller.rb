3d0c09c790b238dcd5a6d530f21b34ea2696c56c
chonasan <tk24120824@gmail.com>
2021-03-20 04:08:21 +0000

    first

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,2 +1,21 @@
 class Public::CustomersController < Public::ApplicationController
+    
+   def edit
+   end
+    
+    
+   def show
+   end
+    
+   def update
+   end
+   
+   def unsubscribe
+   end
+   
+   def withdraw
+   end
+    
+    
+    
 end
--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,3 +1,23 @@
 class Public::ItemsController < Public::ApplicationController
   before_action :authenticate_customer!,except: [:index, :show]
+  
+    def index
+      @items = Item.all
+      @items = Item.page(params[:page]).reverse_order
+      @items = Item.page(params[:page]).per(8)
+    end
+
+    def show
+      @item = Item.find(params[:id])
+      @newbook = Book.new
+      @cart_item = CartItem.new
+    end
+
+
+     private
+     def item_params
+      params.require(:item).permit(:name, :introduction, :price, :image)
+     end
+
+  
 end
