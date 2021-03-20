f37d182713dd9781b468ae4e1fec44f07f66d2d1
chonasan <tk24120824@gmail.com>
2021-03-20 15:09:47 +0000

    secound

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -31,9 +31,9 @@ class Public::CustomersController < Public::ApplicationController
    
     private
 
-      def customer_params
-        params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postal_code, :address, :telephone_number, :is_deleted)
-      end
+    def customer_params
+     params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postal_code, :address, :telephone_number, :is_deleted)
+    end
 
     
     
