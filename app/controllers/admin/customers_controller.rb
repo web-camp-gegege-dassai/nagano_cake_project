3bd554bafa21773f7f8cf2126bf1f55cc9db2485
Takuya3627 <tm.0630.ppp@gmail.com>
2021-03-21 09:06:15 +0000

    Add Commit

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,3 +1,21 @@
 class Admin::CustomersController < Admin::ApplicationController
+    
+  def index
+    @customers = Customer.page(params[:page]).reverse_order
+  end
+  
+  def show
+    @customer = Customer.find(params[:id])
+  end
+  
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+
+  private
+  
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postal_code, :address, :telephone_number, :is_deleted)
+  end
 
 end
