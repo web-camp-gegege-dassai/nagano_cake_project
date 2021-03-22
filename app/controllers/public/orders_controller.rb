8cf6c640c8ba8a07b210912b78ba5ba4cb91e821
ryouichi0618 <tennis.ryouichi@gmail.com>
2021-03-22 16:24:16 +0000

    [Add]orderの機能を追加（仮）

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,21 +1,39 @@
 class Public::OrdersController < Public::ApplicationController
-
+  # before_action :ensure_correct_order, only: [:show]
+  
   def new
     @order = Order.new
     @address = Address.where(customer: current_customer)
   end
 
   def confirm
-    @cart_items = current_cart
+    @cart_items = current_customer.cart_items
     @order = Order.new(
       customer: current_customer,
       payment_method: params[:order][:payment_method]
-      )
+    )
+    @order.shipping_cost = 800
+    @order.total_payment = billing(@order)
+    @selected_address = params[:order][:shipping_address]
+    
+
+    if @selected_address == "my_address"
+      @order.postal_code = current_customer.postal_code
+      @order.address = current_customer.address
+      @order.name = current_customer.name
       
-      @order.total_payment = billing(@order)
+    elsif @selected_address == "registered_address"
+      registered = Address.find(params[:order][:address_id])
+      @order.postal_code = registered.postal_code
+      @order.address = registered.address
+      @order.name = registered.name
       
-      if params[:order][:shipping_address] == "my_address"
-      end
+    elsif @selected_address == "new_address"
+      @order.postal_code = params[:order][:postal_code]
+      @order.address = params[:order][:address]
+      @order.name = params[:order][:name]
+      @address_new = 1
+    end
   end
 
 
@@ -23,24 +41,45 @@ class Public::OrdersController < Public::ApplicationController
   end
 
   def create
+    @order = current_customer.orders.create(order_params)
+    
+    if params[:order][:address_new] == "1"
+      current_customer.address.create(address_params)
+    end
+    
+    @cart_items = current_customer.cart_items
+    @cart_items.each do |cart_item|
+      OrderDetail.create(
+        order: @order,
+        item: cart_item.item,
+        amount: cart_item.amount,
+        # 小計の計算(仮)
+        price: sub_price(cart_item)
+        )
+    end
+    @cart_items.destroy_all
   end
 
   def index
+    @orders = current_customer.orders
   end
 
   def show
+    @order = Order.find(params[:id])
   end
 
+  private
+  
+  def order_params
+    params.require(:order).permit(:postal_code, :address, :name, :payment_method, :total_price)
+  end
+  
+  def address_params
+    params.require(:order).permit(:postal_code, :adress, :name)
+  end
+
+  # def ensure_correct_order
+  #   redirect_to cart_items_path if params[:id] == "confirm"
+  # end
 
 end
-
-
-
-
-def total_price(totals)
-    price = 0
-    totals.each do |total|
-      price  +=  sub_price(total)
-    end
-    return price
-  end
