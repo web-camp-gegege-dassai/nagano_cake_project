65a15cbf0d5af30c5716c5206acbd600b3131f4d
ryouichi0618 <tennis.ryouichi@gmail.com>
2021-03-23 11:56:28 +0000

    コミット

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,22 +2,5 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
   
-  def tax_price(price)
-    (price * 1.1).floor
-  end
-  
-  
-  def sub_price(sub)
-    (tax_price(sub.item.price) * sub.amount)
-  end
-
-
-  def total_price(totals)
-    price = 0
-    totals.each do |total|
-        price  +=  sub_price(total)
-    end
-    return price
-  end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,5 +10,4 @@ module ApplicationHelper
   def devise_mapping
    @devise_mapping ||= Devise.mappings[:customer]
   end
-
 end
