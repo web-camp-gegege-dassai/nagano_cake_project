119112d0719b0d91e136f2fe9d51fb8e988e696f
Takuya3627 <tm.0630.ppp@gmail.com>
2021-03-24 09:39:05 +0000

    レイアウト変更

--- db/migrate/20210317125611_create_items.rb
+++ db/migrate/20210317125611_create_items.rb
@@ -6,7 +6,7 @@ class CreateItems < ActiveRecord::Migration[5.0]
       t.string :image_id
       t.text :introduction
       t.integer :price
-      t.boolean :is_active
+      t.boolean :is_active, default: false, null: false
 
       t.timestamps
     end
