02ce5c20fc808dc4e054bd11326ce6475d37c4d2
ryouichi0618 <tennis.ryouichi@gmail.com>
2021-03-25 09:04:46 +0000

    [Update]order,address機能の調整

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,5 +1,8 @@
 class Address < ApplicationRecord
   belongs_to :customer
+  
+  validates :customer_id, :name, :address, presence: true
+	validates :postal_code, length: {is: 7}, numericality: { only_integer: true }
 
   def order_address
     self.postal_code + self.address + "　#{self.name}"
